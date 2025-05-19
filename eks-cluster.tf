76ccad4524de33130e1af6e521572beb06b88241
Whykay243 <yinka.ajibola0011@gmail.com>
2025-05-18 21:09:57 -0400

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,34 +1,39 @@
+# Configure the Kubernetes provider using EKS cluster details
 provider "kubernetes" {
-  #load_config_file = "false"
-  host = data.aws_eks_cluster.app-cluster.endpoint
-  token = data.aws_eks_cluster_auth.app-cluster.token
+  host                   = data.aws_eks_cluster.app-cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.app-cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.app-cluster.certificate_authority.0.data)
 }
 
+# Get EKS cluster information
 data "aws_eks_cluster" "app-cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
-
+# Get authentication token for EKS cluster
 data "aws_eks_cluster_auth" "app-cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
+
+# Output EKS cluster ID
 output "cluster_id" {
   value = data.aws_eks_cluster.app-cluster.id
 }
 
+# EKS module from the Terraform AWS registry
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.36.0"
 
-  cluster_name = "app-eks-cluster"
+  cluster_name    = "app-eks-cluster"
   cluster_version = "1.31"
 
   subnet_ids = module.myapp-vpc.private_subnets
-  vpc_id = module.myapp-vpc.vpc_id
-  cluster_endpoint_public_access = true
+  vpc_id     = module.myapp-vpc.vpc_id
+
+  cluster_endpoint_public_access          = true
   enable_cluster_creator_admin_permissions = true
 
   tags = {
@@ -37,6 +42,7 @@ module "eks" {
     team        = "devops"
   }
 
+  # Create a managed node group
   eks_managed_node_groups = {
     worker-nodes = {
       min_size     = 1
@@ -44,8 +50,9 @@ module "eks" {
       desired_size = 3
 
       instance_types = ["t2.small"]
-      key_name       = "may_key"
+      key_name       = "whykayKP"
     }
   }
+
   depends_on = [module.myapp-vpc]
 }
